08801ff90cc03cac1fa397127a91c7421bd1e0bb
ShubhamVerma453 <sv4537373@gmail.com>
2023-02-12 14:24:37 +0530

    adding and removeing setup daone

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import Header from "./components/Header";
 import Note from "./components/Note";
 import Footer from "./components/Footer";
 import noteContent from "./components/noteContent"
+import NewNote from "./components/NewNote";
 
 function App (){
     return <div>
         <Header />
-        
+        <NewNote />        
         {noteContent.map(data =>{
             return <Note 
             key = {data.key}
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -4,6 +4,7 @@ function Note(prop) {
     return <div className="note">
         <h1>{prop.title}</h1>
         <p>{prop.content}</p>
+        <button>DELETE</button>
         </div>
 }
 
